7732ae372f2059dcea092fab09572d503d13b91e
Michael Senn <michael@senn.email>
2017-10-11 11:23:35 +0200

    Add spring skeleton.

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# ESE Team 5
+# ShipMe (ESE Team 5)
 
 Bliblablo.
 
 ![Bild](doc/assets/yeah.gif)
+
+## Getting started
+
+- Clone repository
+- Install gradle
+- Compile application, build uber-jar: `gradle build`
+- Run application: `java -jar build/libs/shipme-$VERSION.jar`
---
+++ src/main/java/ch/morrolan/shipme/HelloController.java
@@ -0,0 +1,14 @@
+package ch.morrolan.shipme;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "ShipMe, at your disposal.";
+    }
+
+}
